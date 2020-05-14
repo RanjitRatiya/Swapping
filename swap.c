8e20e4bbd3f383053ba7ba9896cb41ba6e6ca610
RanjitRatiya <64835151+RanjitRatiya@users.noreply.github.com>
2020-05-14 17:13:08 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,22 @@
+//Program to Swapping of 2 Values, without use of extra variable
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Before Swapping \n");      //We can understand values before swapping
+    printf("Enter First Number : ");
+    scanf("%d",&num1);                //Take 1st Number From the User
+    printf("Enter Second Number : ");
+    scanf("%d",&num2);            //Take 2nd Number From the User
+    
+    //Logic for swapping values
+    num1=num1-num2; 
+    num2=num1+num2;
+    num1=num2-num1;
+    
+    printf("After Swapping \n");      //shows swapped values 
+    printf("First Number : %d \n",num1);  
+    printf("Second Number : %d",num2);
+}
